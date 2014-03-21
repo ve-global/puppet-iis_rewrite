ef706fb3baa4d0273bd11b19072c72c11ada4792
Paul Stack <public@paulstack.co.uk>
2014-03-21 15:52:54 +0000

    Update params.pp
    triggering the travis build

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,6 @@
 # Defaut parameter values for the IIS Rewrite 2.0 module
 
 class iis_rewrite::params {
-
   $package_source_location = $::architecure ? {
     'x86'   => 'http://go.microsoft.com/?linkid=9722533',
     'x64'   => 'http://go.microsoft.com/?linkid=9722532',
