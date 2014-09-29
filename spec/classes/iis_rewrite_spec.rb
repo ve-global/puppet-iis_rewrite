325190034898853c1161f8fe14da5b62ae71b3a1
liamjbennett <lbennett@opentable.com>
2014-09-29 13:55:56 +0100

    refactoring tests

--- spec/classes/iis_rewrite_spec.rb
+++ spec/classes/iis_rewrite_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe 'iis_rewrite', :type => :class do
 
-  let(:facts) { {
+  context 'using default params' do
+    let(:facts) {{
       :osfamily  => 'Windows',
       :architecture => 'x64'
-  } }
-  let(:params) {{
+    }}
+    let(:params) {{
       :package_source_location => 'http://download.microsoft.com/download/6/7/D/67D80164-7DD0-48AF-86E3-DE7A182D6815/rewrite_2.0_rtw_x64.msi',
       :download_destination    => 'C:\temp'
-  }}
-
-  it { should contain_class('iis_rewrite::install').that_comes_before('iis_rewrite') }
-
-  context 'using default params' do
+    }}
 
     it { should contain_class('iis_rewrite') }
+    it { should contain_class('iis_rewrite::install').that_comes_before('iis_rewrite') }
 
     it { should contain_exec('install_iis_7_rewrite_module_2').with({
       'command'  => 'msiexec /I C:\temp\rewrite_2.0_rtw_x64.msi /q',
@@ -35,7 +33,11 @@ describe 'iis_rewrite', :type => :class do
   end
 
   context 'when trying to install on Ubuntu' do
-    let(:facts) { {:osfamily => 'Ubuntu'} }
+    let(:facts) {{
+      :osfamily => 'Ubuntu',
+      :architecture => 'x64'
+    }}
+
     it do
       expect {
         should contain_class('iis_rewrite')
@@ -44,7 +46,11 @@ describe 'iis_rewrite', :type => :class do
   end
 
   context 'when trying to install on RedHat' do
-    let(:facts) { {:osfamily => 'RedHat'} }
+    let(:facts) {{
+      :osfamily => 'RedHat',
+      :architecture => 'x64'
+    }}
+
     it do
       expect {
         should contain_class('iis_rewrite')
